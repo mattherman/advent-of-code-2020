112f8105b4024c1e0785edefa2557566c338b80b
Matthew Herman <mattherman11@gmail.com>
2020-12-01 11:56:36 -0600

    Day 1

---
+++ 01/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expenses = File.ReadAllLines(args[0]);
+            for (int i = 0; i < expenses.Length; i++)
+            {
+                var firstValue = int.Parse(expenses[i]);
+                for (int j = i + 1; j < expenses.Length; j++)
+                {
+                    var secondValue = int.Parse(expenses[j]);
+                    if (firstValue + secondValue == 2020)
+                    {
+                        Console.WriteLine(firstValue * secondValue);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 01/Exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expenses = File.ReadAllLines(args[0]);
+
+            for (int i = 0; i < expenses.Length; i++)
+            {
+                var firstValue = int.Parse(expenses[i]);
+                for (int j = i + 1; j < expenses.Length; j++)
+                {
+                    var secondValue = int.Parse(expenses[j]);
+                    for (int k = j + 1; k < expenses.Length; k++)
+                    {
+                        var thirdValue = int.Parse(expenses[k]);
+                        if (firstValue + secondValue + thirdValue == 2020)
+                        {
+                            Console.WriteLine(firstValue * secondValue * thirdValue);
+                            Environment.Exit(0);
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
