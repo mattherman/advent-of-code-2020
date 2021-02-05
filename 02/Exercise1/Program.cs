320ba4fe4b9eb38841766bdea9790c0a34ba7ead
Matthew Herman <mattherman11@gmail.com>
2021-02-05 13:43:30 -0600

    Day 2

---
+++ 02/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            var validPasswordCount = 0;
+            foreach (var line in lines) {
+                var lineParts = line.Split(" ");
+                var rangeParts = lineParts[0].Split("-");
+                var min = int.Parse(rangeParts[0]);
+                var max = int.Parse(rangeParts[1]);
+                var character = lineParts[1][0];
+                var password = lineParts[2];
+                var characterCount = password.ToCharArray().Count(c => c == character);
+                if (characterCount >= min && characterCount <= max) validPasswordCount++;
+            }
+            Console.WriteLine(validPasswordCount);
+        }
+    }
+}
---
+++ 02/Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            var validPasswordCount = 0;
+            foreach (var line in lines) {
+                var lineParts = line.Split(" ");
+                var positionParts = lineParts[0].Split("-");
+                var firstPosition = int.Parse(positionParts[0]);
+                var secondPosition = int.Parse(positionParts[1]);
+                var character = lineParts[1][0];
+                var password = lineParts[2];
+                var characterCount = password.ToCharArray().Count(c => c == character);
+                if (password[firstPosition-1] == character ^ password[secondPosition-1] == character) validPasswordCount++;
+            }
+            Console.WriteLine(validPasswordCount);
+        }
+    }
+}
